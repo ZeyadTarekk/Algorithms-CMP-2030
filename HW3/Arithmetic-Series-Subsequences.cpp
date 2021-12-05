d4ec48c6dd033412b78d68e48778ce17026ac3d6
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-06 00:04:17 +0200

    ALL true except 3 cases

--- HW3/Arithmetic-Series-Subsequences.cpp
+++ HW3/Arithmetic-Series-Subsequences.cpp
@@ -4,71 +4,77 @@
 #include<vector>
 using namespace std;
 
+bool func(int index, const vector<int>& vec, int& no) {
+    bool flag = false;
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] > index) {
+            no++;
+            flag = true;
+        }    
+    }
+    return flag;
+}
+
 int main() {
-	unordered_map<int, int> myMap;
-	int n, d, m;
-	cin >> n >> d >> m;
-	int no;
-	
-	vector<int> numbers(n);
-	for (int i = 0; i < n; i++) {
-		cin >> no;
-		if (myMap[no] == 0)
-			myMap[no] = 1;
-		else
-			myMap[no]++;
-		numbers[i] = no;
-	}
 
-	//for (auto m:myMap) {
-	//	cout << m.first << " " << m.second << endl;
-	//}
-	unordered_map<int, int>::iterator it;
-	it = myMap.begin();
-	//cout << myMap.size();
-	//vector<vector<int>> ways;
-	vector<int> ways;
-	vector<int>dummyV;
-	int dummy;
-	for (auto it:myMap) {
-		int no1 = 0;
-		vector<int>dummyV;
-		dummy = (it).first;
-		//cout << (*it).first << endl;
-		for (int j = 0; j < m; j++) {
-			dummyV.push_back(dummy);
-			dummy += d;
-		}
-		//ways.push_back()
+    //vector<int> vec({ 1,2,3,4 ,7});
+    //cout << func(8, vec);
 
-		bool flag = true;
-		for (int j = 0; j < dummyV.size(); j++) {
-			if (myMap[dummyV[j]] == 0) {
-				flag = false;
-				break;
-			}
-				
-		}
-		if (flag) {
-			for (int j = 0; j < dummyV.size(); j++) {
-				if (myMap[dummyV[j]] != 0) {
-					if (no1 == 0)
-						no1 += myMap[dummyV[j]];
-					else
-						no1 *= myMap[dummyV[j]];
-				}
-			}
-			ways.push_back(no1);
-		}
-		
-		// it++;
+    unordered_map<int, vector<int>> myMap;
+    int n, d, m;
+    cin >> n >> d >> m;
+    int read;
+    for (int i = 0; i < n; i++) {
+        cin >> read;
+        myMap[read].push_back(i);
+    }
+    //for (auto m:myMap) {
+    //    cout << m.first << " ";
+    //    for (int i = 0; i < m.second.size(); i++)
+    //        cout << m.second[i] << " ";
+    //    cout << endl;
+    //}
+    int no = 0;
+    int dummy;
+    vector<int>waysNo;
+    for (auto mm : myMap) {
+        
+        for (int i = 0; i < mm.second.size(); i++) {
+            int no2 = 0;
+            dummy = mm.first;
+            bool flag = true;
+            for (int j = 0; j < m-1; j++) {
+                if (myMap.find( dummy + d) == myMap.end())
+                    flag = false;
+                dummy += d;
+            }
+            dummy = mm.first;
+            if (flag) {
+                vector<int> numbers;
+                bool flag2 = true;
+                for (int j = 0; j < m - 1; j++) {
+                    no2 = 0;
+                    if (!func(mm.second[i], myMap[dummy + d], no2))
+                        flag2 = false;
+                    else
+                        numbers.push_back(no2);
+                    dummy += d;
+                }
+                if (flag2) {
+                    no = 1;
+                    for (int i = 0; i < numbers.size(); i++)
+                        no *= numbers[i];
+                    waysNo.push_back(no);
+                }
+                    
+            }
 
-	}
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < waysNo.size(); i++)
+        sum += waysNo[i];
+    cout << sum;
 
-	int sum = 0;
-	for (int i = 0; i < ways.size(); i++) {
-		sum += ways[i];
-	}
-	cout << sum;
-	return 0;
+    return 0;
 }
