067db0cfaa4f166f4e22832fb8cc10a78bc2b5b7
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-06 19:39:54 +0200

    ACCEPTED

--- HW3/Arithmetic-Series-Subsequences.cpp
+++ HW3/Arithmetic-Series-Subsequences.cpp
@@ -4,77 +4,54 @@
 #include<vector>
 using namespace std;
 
-bool func(int index, const vector<int>& vec, int& no) {
-    bool flag = false;
-    for (int i = 0; i < vec.size(); i++) {
-        if (vec[i] > index) {
-            no++;
-            flag = true;
-        }    
-    }
-    return flag;
+bool func(int index, const vector<int>& vec) {
+	bool flag = false;
+	for (int i = 0; i < vec.size(); i++) {
+		if (vec[i] > index) {
+			flag = true;
+		}
+	}
+	return flag;
 }
 
 int main() {
 
-    //vector<int> vec({ 1,2,3,4 ,7});
-    //cout << func(8, vec);
+	//vector<int> vec({ 1,2,3,4 ,7});
+	//cout << func(8, vec);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    unordered_map<int, vector<int>> myMap;
-    int n, d, m;
-    cin >> n >> d >> m;
-    int read;
-    for (int i = 0; i < n; i++) {
-        cin >> read;
-        myMap[read].push_back(i);
-    }
-    //for (auto m:myMap) {
-    //    cout << m.first << " ";
-    //    for (int i = 0; i < m.second.size(); i++)
-    //        cout << m.second[i] << " ";
-    //    cout << endl;
-    //}
-    int no = 0;
-    int dummy;
-    vector<int>waysNo;
-    for (auto mm : myMap) {
-        
-        for (int i = 0; i < mm.second.size(); i++) {
-            int no2 = 0;
-            dummy = mm.first;
-            bool flag = true;
-            for (int j = 0; j < m-1; j++) {
-                if (myMap.find( dummy + d) == myMap.end())
-                    flag = false;
-                dummy += d;
-            }
-            dummy = mm.first;
-            if (flag) {
-                vector<int> numbers;
-                bool flag2 = true;
-                for (int j = 0; j < m - 1; j++) {
-                    no2 = 0;
-                    if (!func(mm.second[i], myMap[dummy + d], no2))
-                        flag2 = false;
-                    else
-                        numbers.push_back(no2);
-                    dummy += d;
-                }
-                if (flag2) {
-                    no = 1;
-                    for (int i = 0; i < numbers.size(); i++)
-                        no *= numbers[i];
-                    waysNo.push_back(no);
-                }
-                    
-            }
+	unordered_map<int, int> myMap;
+	unordered_map<int, int> myMap2;
+	int n, d, m;
+	cin >> n >> d >> m;
+	int read;
+	
+	int no = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> read;
+		if (myMap.find(read) == myMap.end())
+			myMap[read] = 1;
+		else
+			myMap[read]++;
+		if (m == 2) {
+			if (myMap[read - d] != 0)
+				no += myMap[read - d];
+		}
+		else if (m == 3) {
+			if (myMap[read - d] != 0) {
+				myMap2[read] += myMap[read - d];
+				if (myMap2[read - d] != 0)
+					no += myMap2[read - d];
+			}
+		}
+	}
+	cout << no;
 
-        }
-    }
-    int sum = 0;
-    for (int i = 0; i < waysNo.size(); i++)
-        sum += waysNo[i];
-    cout << sum;
 
-    return 0;
+
+
+
+	return 0;
 }
