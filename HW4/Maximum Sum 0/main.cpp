b7fbd15ba682dc400ce0ebe059f3657d52c8d0c8
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-27 13:19:43 +0200

    Maximum Sum 0

--- HW4/Maximum Sum 0/main.cpp
+++ HW4/Maximum Sum 0/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <vector>
 #include <iostream>
+#include <climits>
 #include <algorithm>
 using namespace std;
 
@@ -10,15 +11,33 @@ using namespace std;
 struct Node
 {
     int index;
-    int rating;
+    long long rating;
     Node* parent; // this is optional
     vector<Node*> children;
 };
 
 
-long long maximumSum(Node* root) {
+long long maximumSum(Node* root, vector<long long>& memo) {
+  if (root == nullptr)
+    return 0;
 
-  return 0;
+  if (memo[root->index] != -1)
+    return memo[root->index];
+
+  long long maxv1=0, maxv2=0;
+  for (int i = 0; i < root->children.size(); i++) {
+    for (int j = 0; j < root->children[i]->children.size(); j++) {
+      maxv1 +=  maximumSum(root->children[i]->children[j],memo);
+    }
+  }
+  maxv1 += root->rating;
+
+
+  for (int i = 0; i < root->children.size(); i++) {
+    maxv2 += maximumSum(root->children[i],memo);
+  }
+  memo[root->index] = max(maxv2, maxv1);
+  return memo[root->index];
 }
 
 int main() {
@@ -36,7 +55,7 @@ int main() {
 
   // Iterating over all edges
   int parent, child;
-  for (int i = 0; i < N-1; i++) {
+  for (int i = 0; i < N - 1; i++) {
     cin >> parent >> child;
     nodes[child]->parent = nodes[parent];
     nodes[parent]->children.push_back(nodes[child]);
@@ -44,6 +63,11 @@ int main() {
 
   // TODO: Create a vector called 'memo' used for memoization (What is the dimensions of that vector?)
   // then uncomment the following line.
+
+  vector<long long> memo(N,-1);
+
+
+  cout << maximumSum(nodes[0], memo);
   // cout << maximumSum(nodes[0], memo);
 
   return 0;
